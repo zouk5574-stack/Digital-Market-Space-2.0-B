09606afb7814a8d697804be70cf9a4d5b2fa4818
zouk5574-stack <zouk5574@gmail.com>
2025-10-06 17:12:42 +0100

    Update freelanceRoutes.js

--- routes/freelanceRoutes.js
+++ routes/freelanceRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "/middleware/authMiddleware.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 import {
   createFreelanceMission,
   applyToMission,
